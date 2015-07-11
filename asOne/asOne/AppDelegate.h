073990176f6a7eec6a5314240bc48e8203ee4f31
JoshJSZ <joshjiav5@gmail.com>
2015-07-11 12:02:08 -0700

    Empty Xcode Project
    Init check in

---
+++ asOne/asOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  asOne
+//
+//  Created by JoshJSZ on 7/11/15.
+//  Copyright (c) 2015 asOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ asOne/asOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  asOne
+//
+//  Created by JoshJSZ on 7/11/15.
+//  Copyright (c) 2015 asOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

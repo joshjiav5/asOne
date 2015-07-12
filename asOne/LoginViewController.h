89253ca158b24b593782c3f162f8cd008d4e9f86
Ibrahim Sheikh <oprah@nestlabs.com>
2015-07-11 20:10:34 -0700

    some facebook

---
+++ asOne/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  asOne
+//
+//  Created by oprah on 7/11/15.
+//  Copyright (c) 2015 asOne. All rights reserved.
+//
+
+#import "MasterViewController.h"
+
+@interface LoginViewController : MasterViewController
+
+@end
+
+
---
+++ asOne/LoginViewController.m
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.m
+//  asOne
+//
+//  Created by oprah on 7/11/15.
+//  Copyright (c) 2015 asOne. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <Parse/Parse.h>
+#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+#import <Parse>
+
+@implementation LoginViewController
+
+- (void)_loginWithFacebook {
+    // Set permissions required from the facebook user account
+    NSArray *permissionsArray = @[ @"user_about_me", @"user_relationships", @"user_birthday", @"user_location"];
+    
+    // Login PFUser using Facebook
+    [PFFacebookUtils logInInBackgroundWithReadPermissions:permissionsArray block:^(PFUser *user, NSError *error) {
+        if (!user) {
+            NSLog(@"Uh oh. The user cancelled the Facebook login.");
+        } else if (user.isNew) {
+            NSLog(@"User signed up and logged in through Facebook!");
+        } else {
+            NSLog(@"User logged in through Facebook!");
+        }
+    }];
+}
+
+- (void)viewDidLoad {
+    NSLog(@"hello");
+    PFlO
+}
+
+@end

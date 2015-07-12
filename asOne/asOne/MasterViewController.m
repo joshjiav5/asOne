2a523b8e6bf7f3242b3154fe3be41a5ded5a5588
JoshJSZ <joshjiav5@gmail.com>
2015-07-11 19:01:13 -0700

    Now its working

--- asOne/asOne/MasterViewController.m
+++ asOne/asOne/MasterViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"background"]];
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"viewBackground"]];
+    
 }
 
 - (void)didReceiveMemoryWarning {
